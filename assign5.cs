f6fb1261954ff5a6e42b73cd7ac8a6dd36ed7e5a
Ananya Anand <90851564+ananya24anand@users.noreply.github.com>
2022-08-23 17:04:45 +0530

    Add files via upload

---
+++ assign5.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment
+{
+    internal class assign5
+    {
+        public static string first_last(string ustr)
+        {
+           int len = ustr.Length;
+            return ustr[len - 1] + ustr.Substring(1, len - 2) + ustr[0];
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string: ");
+            string ustr = first_last(Console.ReadLine());
+            Console.WriteLine($"new String: {ustr}");
+
+        }
+       
+    }
+}
