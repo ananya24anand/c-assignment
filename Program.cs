1f736d884a5972a0999f2aa227ba4e57ca3213a9
Ananya Anand <90851564+ananya24anand@users.noreply.github.com>
2022-08-23 09:33:18 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            int num2;
+            int num3;
+            int result;
+
+            Console.WriteLine("Enter the 3 numbers : ");
+
+            num1 = int.Parse(Console.ReadLine());
+            num2 = int.Parse(Console.ReadLine());  
+            num3 = int.Parse(Console.ReadLine());
+
+            result = num1 * num2 * num3;
+
+         
+            Console.WriteLine("The multiplication result is " + result.ToString());
+
+        }
+    }
+}
